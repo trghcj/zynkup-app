1f9e1f51b3546410691ae343a860b2d39da2ab6c
trghcj <164627905+trghcj@users.noreply.github.com>
2026-05-01 18:53:43 +0530

    fix: correct cors configuration

--- zynkup_backend/app/main.py
+++ zynkup_backend/app/main.py
@@ -42,7 +42,7 @@ ALL_ORIGINS = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=ALL_ORIGINS, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
