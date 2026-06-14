c69bb16062b86abc101e0f8c6cf68c9afe9bf4e6
trghcj <164627905+trghcj@users.noreply.github.com>
2026-06-14 07:41:26 +0530

    fix: Clear build cache and force inclusion of zynkup_logo.jpg in web bundle

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2616722312" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1789645082" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
