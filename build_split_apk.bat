feebbad4d16e3ab8388fc8f0325668624e84824d
trghcj <164627905+trghcj@users.noreply.github.com>
2026-07-21 16:30:57 +0530

    chore: Remove study guide and untrack build_split_apk scripts

--- build_split_apk.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Building APKs per architecture...
-call flutter build apk --split-per-abi
-echo Done! Compressed APKs are located in build\app\outputs\apk\release\
--- build_split_apk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Building APKs per architecture..."
-flutter build apk --split-per-abi
-echo "Done! Compressed APKs are located in build/app/outputs/apk/release/"
